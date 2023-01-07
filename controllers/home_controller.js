032d99e6db932a6f7dbcad6b423f6da61c0c8384
Vishal-pandey17 <vishalpandey86681@gmail.com>
2023-01-07 13:55:25 +0530

    Display posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,24 @@
+const Post = require('../models/post');
+const { populate } = require('../models/user');
+
 // A group of action function in one file is called controller
 module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);                // Update that presented cookie
-    return res.render('home', {
-        title: "Home"
-    });
+    /*Post.find({}, function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
+    })*/
+
+    Post.find({}, populate('user'), exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
+    }))
+    
 }
 
 // module.exports.actionName = function(req, res){}
