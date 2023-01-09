16aa93d055cdb6b987dce5539abbc02528d44755
Vishal-pandey17 <vishalpandey86681@gmail.com>
2023-01-10 01:10:41 +0530

    CURD Part is completed

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -19,7 +19,7 @@ module.exports.create = function(req, res){
              post.comments.push(comment);
              post.save();
 
-             res.redirect('/');
+             res.redirect('/');  // bring to home page..
         });
      }
    });       
