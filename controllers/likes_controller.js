828568d0c056878eaadc806e3c06ed6cabd257b7
Vishal-pandey17 <vishalpandey86681@gmail.com>
2023-01-19 19:29:20 +0530

    Actions and Routes::Likes added Successfully

---
+++ controllers/likes_controller.js
@@ -0,0 +1,58 @@
+const Like = require('../models/like');
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+
+module.exports.toggleLike = async function(req, res){
+   try{
+    // url like --> likes/toggle/?id=abcdef&type=Post or Comment
+    let likeable;
+    let deleted = false;
+    if(req.query.type == 'Post'){
+        likeable = await Post.findById(req.query.id).populate('likes'); // If there are already like in the likes array then i populate the likes
+
+    }else{
+        likeable = await Comment.findById(req.query.id).populate('likes');
+    }
+
+    // Check if like is already exsist
+    let existingLike = await Like.findOne({
+        likeable: req.query.id,
+        onModel: req.query.type,
+        user: req.user._id
+    })
+
+    // if like already exists then delete it 
+    if(existingLike){
+        likeable.likes.pull(existingLike._id);
+        likeable.save();
+
+        existingLike.remove();
+        deleted = true;
+    }else{
+        // Make a new like
+        let newLike = await Like.create({
+            user: req.user._id,
+            likeable: req.query._id,
+            onModel: req.query.type
+            
+        });
+
+        likeable.likes.push(like._id);
+        likeable.save();
+    }
+
+    return res.json({
+        message: 'Request Successfull!',
+
+    })
+       
+   }catch(err){
+      console.log(err);
+      return res.json(500, {
+        message: 'Internal Server Error!!',
+        data: {
+            deleted: deleted
+        }
+      });
+   }
+}
