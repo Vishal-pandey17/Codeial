d472c5dbda61e228004e25faf55c348917aa74c5
Vishal-pandey17 <vishalpandey86681@gmail.com>
2023-01-26 16:51:44 +0530

    Connecting  the socket

--- assest/js/chat_engine.js
+++ assest/js/chat_engine.js
@@ -1,1 +1,18 @@
 // This is client side
+class chatEngine{
+    constructor(chatBoxId, userEmail){
+        this.chatBox = $(`${chatBoxId}`);
+        this.userEmail = userEmail;
+        // run this server in specified port the io file is given by cdn socket file
+        this.socket = io.connect('http://localhost:5000');
+        if(this.userEmail){
+            connectionHandler();
+        }
+    }
+
+    connectionHandler(){
+       this.socket.on('connect', function(){
+          console.log("connection established using sockets...!")
+       });
+    }
+}
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,5 +1,12 @@
 // this is observer receive the incomming from the users
 module.exports.chatSockets = function(socketServer){
-     
+     let io = require('socket.io')(socketServer);
 
+     io.sockets.on('connection', function(socket){
+        console.log("New connection received", socket.id);
+
+        socket.on('disconnect', function(){
+            console.log("sockt disconnected..!");
+        });
+     });
 }
